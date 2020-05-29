a7b200d2da5e77dbeef560ba6777f6182b80a9e5
nano-2-ly <dong991207@unist.ac.kr>
2020-05-30 02:53:09 +0900

    robot arm communication upgrade

--- test.py
+++ test.py
@@ -3,12 +3,36 @@ from pygame import mixer
 import wave
 import json
 import STT
+import socket
+
+class IOT():
+    def __init__(self):
+        self.HOST = '127.0.0.1'                 # Symbolic name meaning all available interfaces
+        self.PORT = 8005           # Arbitrary non-privileged port
+        
+    def turn_on_light(self, light_ID):
+        if light_ID == 'room':
+            import socket
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.HOST,self.PORT))
+            s.sendall('room_light_on'.encode('utf-8'))
+            s.close()
+            
+    
+    def turn_off_light(self, light_ID):
+        if light_ID == 'room':
+            import socket
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.HOST,self.PORT))
+            s.sendall('room_light_off'.encode('utf-8'))
+            s.close()
+            
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 5
+RECORD_SECONDS = 3
 WAVE_OUTPUT_FILENAME = "./sounds/output.wav"
 
 
@@ -49,3 +73,9 @@ wf.close()
 
 command = json.loads(STT.naver_STT('./sounds/output.wav'))['text']
 print(command)
+
+if '불 켜' in command : 
+    IOT().turn_on_light('room')
+
+elif '불 꺼' in command : 
+    IOT().turn_off_light('room')
